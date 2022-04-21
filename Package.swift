83024bafd9e3bca65ea7ae5e07b85030e75d905d
Dimitar Chakarov <dimitar.chakarov@just-eat.com>
2022-04-21 21:38:32 +0100

    Add SPM support

---
+++ Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version:5.1
+
+import PackageDescription
+
+let package = Package(
+    name: "AutomationTools",
+    platforms: [
+        .iOS(.v12)
+    ],
+    products: [
+        .library(
+            name: "AutomationTools-Core",
+            targets: ["AutomationTools-Core"]),
+        .library(
+            name: "AutomationTools-Host",
+            targets: ["AutomationTools-Host"]),
+    ],
+    targets: [
+        .target(
+            name: "AutomationTools-Host",
+            path: "AutomationTools/Classes/HostApp/"
+        )
+    ]
+)
